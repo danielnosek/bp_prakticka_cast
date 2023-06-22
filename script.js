dde2336a2d7e8f798b837eeb074a643acf0dd17f
Daniel Nosek <nosek.dan@gmail.com>
2023-06-22 18:24:52 +0200

    V_2

--- script.js
+++ script.js
@@ -1,129 +1,157 @@
 const questions = [
   {
-    question: "Co je to phishing?",
-    options: [
-      "Získávání citlivých informací prostřednictvím falešných emailů",
-      "Útok na síťovou infrastrukturu",
-      "Pokus o fyzický vstup do firmy"
-    ],
-    answer: 0
-  },
-  {
-    question: "Co je to tailgating?",
-    options: [
-      "Zneužití nezabezpečeného přístupového bodu",
-      "Neoprávněný přístup do systému pomocí ukradených přihlašovacích údajů",
-      "Vstup do zabezpečených prostorů za souhlasu zaměstnance"
-    ],
+    question: "Jak se nazývá útok, při kterém útočník vydává za důvěryhodnou osobu či organizaci?",
+    options: ["Phishing", "Malware", "Spoofing", "Social Engineering"],
     answer: 2
   },
   {
-    question: "Co je to pretexting?",
+    question: "Co je to tailgating (černý pasažér)?",
     options: [
-      "Podvodný výmysl za účelem získání informací",
-      "Útok na firemní servery",
-      "Neoprávněné sledování síťového provozu"
+      "Fyzický útok, kdy útočník pronikne do prostor, ke kterým nemá oprávnění",
+      "Útok, při kterém útočník napodobuje známou osobu",
+      "Útok, který se zaměřuje na sociální sítě",
+      "Útok, při kterém útočník využívá zranitelnosti ve firemním softwaru"
     ],
     answer: 0
   },
   {
-    question: "Jakou roli hraje social engineering při útocích na firemní zaměstnance?",
+    question: "Který z následujících je příkladem sociálního inženýrství?",
     options: [
-      "Social engineering není používán při útocích na zaměstnance",
-      "Social engineering je pouze o technických útocích",
-      "Social engineering využívá lidského faktoru k získání informací"
-    ],
-    answer: 2
-  },
-  {
-    question: "Co je to vishing?",
-    options: [
-      "Podvodné získávání informací pomocí telefonního hovoru",
-      "Fyzický útok na zaměstnance",
-      "Útok na firemní databáze"
+      "Odeslání e-mailu s odkazem na falešnou stránku, která vyzývá k zadání citlivých informací",
+      "Využití škodlivého softwaru ke kompromitaci systému",
+      "Provedení síťového skenování",
+      "Napadení firewallu pomocí DDoS útoku"
     ],
     answer: 0
   },
   {
-    question: "Co je to shoulder surfing?",
+    question: "Co znamená pojem 'shoulder surfing'?",
     options: [
-      "Podvodná manipulace s firemním hardwarem",
-      "Fyzické sledování a odposlech zaměstnanců",
-      "Útok na firemní webové stránky"
+      "Přístup k citlivým informacím na sociálních sítích",
+      "Využití fyzické blízkosti ke sledování zadávání citlivých údajů",
+      "Zneužití důvěry zaměstnanců ve firmě",
+      "Využití sociálního inženýrství ke získání hesla"
     ],
     answer: 1
   },
   {
-    question: "Co je to dumpster diving?",
+    question: "Která ze následujících je preventivní opatření proti sociálnímu inženýrství?",
     options: [
-      "Prohledávání odpadků s cílem získat citlivé informace",
-      "Fyzický útok na firemní budovy",
-      "Útok na firemní emailové účty"
+      "Pravidelná aktualizace softwaru a systémů",
+      "Využití silného hesla",
+      "Otevírání podezřelých e-mailů",
+      "Sdílení citlivých informací na sociálních sítích"
     ],
     answer: 0
   },
   {
-    question: "Co je to spear phishing?",
+    question: "Co znamená pojem 'phishing'?",
     options: [
-      "Cílený phishing útok na konkrétní osobu nebo organizaci",
-      "Útok na počítačové sítě pomocí malwaru",
-      "Zneužití zranitelností v softwaru"
+      "Útok, při kterém se útočník vydává za důvěryhodnou osobu či organizaci",
+      "Využití zranitelnosti ve firemním softwaru",
+      "Útok, při kterém útočník napodobuje známou osobu",
+      "Přístup k citlivým informacím na sociálních sítích"
     ],
     answer: 0
   },
   {
-    question: "Co je to baiting?",
+    question: "Co je to 'pretexting'?",
     options: [
-      "Využívání předstírané identity pro získání informací",
-      "Útok na bezdrátové sítě",
-      "Podvodná nabídka nebo slib s cílem získat informace"
-    ],
-    answer: 2
-  },
-  {
-    question: "Co je to quid pro quo?",
-    options: [
-      "Podvodná nabídka výhod nebo služeb výměnou za informace",
-      "Fyzická nátlaková technika",
-      "Útok na počítačové systémy pomocí šifrování"
+      "Útok, při kterém se útočník vydává za jinou osobu a snaží se získat citlivé informace",
+      "Využití škodlivého softwaru ke kompromitaci systému",
+      "Zneužití důvěry zaměstnanců ve firmě",
+      "Fyzický útok, kdy útočník pronikne do prostor, ke kterým nemá oprávnění"
     ],
     answer: 0
+  },
+  {
+    question: "Jakou roli hraje osvěta a školení zaměstnanců při prevenci sociálního inženýrství?",
+    options: [
+      "Nemá žádný vliv na prevenci útoků",
+      "Může snížit riziko úspěšnosti útoků",
+      "Zvyšuje motivaci útočníků",
+      "Je zbytečná, protože útoky jsou nezadržitelné"
+    ],
+    answer: 1
+  },
+  {
+    question: "Co je to 'vishing'?",
+    options: [
+      "Útok, při kterém útočník vydává za důvěryhodnou osobu či organizaci",
+      "Využití telefonu ke kompromitaci systému",
+      "Útok, při kterém útočník napodobuje známou osobu",
+      "Přístup k citlivým informacím na sociálních sítích"
+    ],
+    answer: 1
+  },
+  {
+    question: "Který z následujících je příkladem sociálního inženýrství?",
+    options: [
+      "Využití škodlivého softwaru ke kompromitaci systému",
+      "Vylákání citlivých informací pomocí podvodného telefonního hovoru",
+      "Provedení síťového skenování",
+      "Napadení firewallu pomocí DDoS útoku"
+    ],
+    answer: 1
   }
 ];
 
-let currentQuestion = 0; // Proměnná pro sledování aktuální otázky
-let score = 0; // Proměnná pro sledování počtu správných odpovědí
+let currentQuestion = 0;
+let score = 0;
 
-function loadQuestion() {
-  // Funkce pro načtení otázky do rozhraní
+function displayQuestion() {
+  const questionElement = document.getElementById("question");
+  const optionsElement = document.getElementById("options");
+  const resultElement = document.getElementById("result");
 
-  // Přidáme průběžné počítadlo správných odpovědí
-  const scoreElem = document.getElementById("score");
-  scoreElem.textContent = `Správné odpovědi: ${score}/${currentQuestion}`;
+  questionElement.textContent = questions[currentQuestion].question;
+
+  optionsElement.innerHTML = "";
+  for (let i = 0; i < questions[currentQuestion].options.length; i++) {
+    const option = document.createElement("input");
+    option.setAttribute("type", "radio");
+    option.setAttribute("name", "answer");
+    option.setAttribute("value", i);
+    option.setAttribute("required", true);
+    optionsElement.appendChild(option);
+
+    const label = document.createElement("label");
+    label.textContent = questions[currentQuestion].options[i];
+    optionsElement.appendChild(label);
+
+    optionsElement.appendChild(document.createElement("br"));
+  }
+
+  resultElement.textContent = "";
 }
 
 function checkAnswer() {
-  // Funkce pro ověření zvolené odpovědi
+  const options = document.getElementsByName("answer");
+  let selectedOption;
 
-  // Zvýšíme skóre, pokud je odpověď správná
-  if (selectedAnswer === questions[currentQuestion].answer) {
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].checked) {
+      selectedOption = options[i].value;
+      break;
+    }
+  }
+
+  if (selectedOption == questions[currentQuestion].answer) {
     score++;
   }
 
-  // Zvýšíme pořadí aktuální otázky
   currentQuestion++;
 
-  // Znovu načteme další otázku nebo zobrazíme výsledek, pokud jsou otázky vyčerpány
   if (currentQuestion < questions.length) {
-    loadQuestion();
+    displayQuestion();
   } else {
-    const resultElem = document.getElementById("result");
-    resultElem.textContent = `Váš výsledek: ${score}/${questions.length}`;
+    displayResult();
   }
-
-  // Aktualizujeme průběžné počítadlo správných odpovědí
-  const scoreElem = document.getElementById("score");
-  scoreElem.textContent = `Správné odpovědi: ${score}/${currentQuestion}`;
 }
 
-// Zbytek kódu zůstává stejný
+function displayResult() {
+  const resultElement = document.getElementById("result");
+  resultElement.textContent = "Váš výsledek: " + score + "/" + questions.length;
+}
+
+displayQuestion();
